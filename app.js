9ca677cadbd999e4622d13e49739c347c109afbb
Kartikey <kartikey@Kartikeys-MacBook-Air.local>
2025-08-09 21:59:22 +0530

    first push

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {PORT} from './config/env.js'
+import subscriptionRouter  from "./routes/subscription.routes.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
+
+app.get('/', (req,res) => {
+    res.send("Welcome to the sub tracker api")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server runnig on http://localhost:${PORT}`);
+})
---
+++ routes/subscription.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+subscriptionRouter.get('/', (req,res) => res.send({title: "GET all subscriptions"}))
+
+export default subscriptionRouter
