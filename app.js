4ea939568c22b346bf417f9e649b359d8d1d4d8a
Kartikey <kartikey@Kartikeys-MacBook-Air.local>
2025-08-10 02:47:37 +0530

    err, authentication and authorization done

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ import subscriptionRouter  from "./routes/subscription.routes.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDB from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// built-in middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+
+app.use(errorMiddleware)
 
 app.get('/', (req,res) => {
     res.send("Welcome to the Subscription tracker API!")
