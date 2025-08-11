7ec14110d47b1d3d8b7691ab4ec9572a80f4e6c0
Kartikey <kartikey@Kartikeys-MacBook-Air.local>
2025-08-12 03:03:25 +0530

    working on hostpie but still left to check if works fine

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,1 +1,12 @@
-const transporter = nodema
+import nodemailer from 'nodemailer'
+import { EMAIL_PASSWORD } from './env.js'
+
+export const accEmail = 'kartikeyattri@gmail.com'
+
+export const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user: accEmail,
+        pass: EMAIL_PASSWORD
+    }
+})
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import {createRequire} from 'module'
 import Subscription from '../models/subscription.model.js';
+import { sendReminderEmail } from '../utils/send-email.js';
 const require = createRequire(import.meta.url)
 const{serve} = require('@upstash/workflow/express')
 
@@ -24,7 +25,7 @@ export const sendReminders = serve(async (context) =>{
         if(reminderDate.isAfter(dayjs())){
             await sleepUntilReminder(context, `Reminder ${daysBefore} days before`, reminderDate)
         }
-        await triggerReminder(context, `Reminder ${daysBefore} days before`)
+        await triggerReminder(context, `${daysBefore} days before reminder`, subscription)
     }
 })
 
@@ -39,9 +40,15 @@ const sleepUntilReminder = async (context, label, date) => {
     await context.sleepUntil(label, date.toDate())
 }
 
-const triggerReminder = async (context, label) => {
-    return await context.run(label, () => {
+const triggerReminder = async (context, label,subscription) => {
+    return await context.run(label, async() => {
         console.log(`Trigerring ${label} reminder`);
         // Send email,SMS,push notifications....
+        
+        await sendReminderEmail({
+            to: subscription.user.email,
+            type: label,
+            subscription: subscription
+        })
     })
 }
